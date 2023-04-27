f1ce584ca12d20b29bb0a3b64f8940866b9c57f0
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-27 16:34:34 +0700

    change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     };
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">FINANCE.</h1>
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
@@ -27,7 +27,7 @@ export const Navbar = () => {
             : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">FINANCE.</h1>
         <li className="p-4 border-b border-gray-600">Home</li>
         <li className="p-4 border-b border-gray-600">Company</li>
         <li className="p-4 border-b border-gray-600">Resources</li>
